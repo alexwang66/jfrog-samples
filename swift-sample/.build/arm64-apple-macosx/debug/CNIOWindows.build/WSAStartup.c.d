ea04ed2cab9c21f7a93d70f4ac874fa923c8cc2f
unknown <wq237wq@gmail.com>
2026-09-24 22:58:18 +0800

    Ignore Swift build artifacts

--- swift-sample/.build/arm64-apple-macosx/debug/CNIOWindows.build/WSAStartup.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/qingwang/Documents/workspace/code/jfrog-sample/swift-sample/.build/registry/downloads/apple/swift-nio/2.32.0/Sources/CNIOWindows/include/module.modulemap \
-  /Users/qingwang/Documents/workspace/code/jfrog-sample/swift-sample/.build/registry/downloads/apple/swift-nio/2.32.0/Sources/CNIOWindows/WSAStartup.c
